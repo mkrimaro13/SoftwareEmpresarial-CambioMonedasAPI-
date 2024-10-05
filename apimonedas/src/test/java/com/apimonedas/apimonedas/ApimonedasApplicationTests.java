a644d4bfacccc15ccd5a719fb2127b3065a4f635
Marco Osorio <marco.osorio86@hotmail.com>
2024-10-04 20:49:29 -0500

    First Commit

---
+++ apimonedas/src/test/java/com/apimonedas/apimonedas/ApimonedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimonedas.apimonedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimonedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
